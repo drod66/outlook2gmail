526d1220cc8994383dbbd53bca6800d17c71538d
Daniel Rodrigue <drodrigue@hotmail.com>
2016-02-24 21:52:50 -0500

    Code cleanups
    - move code into [lib](./lib) folder for better reusability
    - added support for more Outlook fields
    - some speed improvements

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-var csv = require('csv'),
-    fs = require('fs'),
-    path = require('path'),
-    stream = require('stream'),
-    untildify = require('untildify'),
-    iconv = require('iconv-lite'),
-    outlook_fields = require('./outlook-fields'),
-    google_fields = require('./google-fields'),
-    outlook2google = require('./outlook2google'),
-    encodings = {
-        'google': 'utf16le',
-        'outlook': 'utf8'
-    };
+var path = require('path'),
+    convert = require('./lib/convert');
 
 (function() {
     'use strict';
@@ -18,58 +7,11 @@ var csv = require('csv'),
     var argv = process.argv;
 
     if (argv.length < 4) {
-        process.stderr.write('Usage: node ' + path.relative('.', argv[1]) + ' ~/Downloads/OutlookContacts.csv output.csv\n');
+        process.stderr.write('Converts a Outlook contacts CSV file into a Google CSV file.');
+        process.stderr.write('Usage: node ' + path.relative('.', argv[1]) + ' ./OutlookContacts.csv ./google.csv\n');
         process.exit(1);
     }
 
-    var index = 0;
-    fs.createReadStream(untildify(argv[2]), {
-            encoding: encodings.outlook
-        })
-        .pipe(csv.parse({
-            delimiter: ',',
-            columns: true,
-            auto_parse: false
-        }))
-        .pipe(csv.transform(function(record) {
-            if (index++ === 0) {
-                // check the columns
-                var unknown = [],
-                    columns = Object.keys(record);
-                columns.forEach(function(name) {
-                    if (outlook_fields.indexOf(name) < 0) {
-                        unknown.push(name);
-                    }
-                });
-                if (unknown.length > 0) {
-                    process.stderr.write('WARNING: Unexpected column(s): ' + unknown.join(', ') + '\n');
-                }
-                return google_fields;
-            } else {
-                var data = [];
-                google_fields.forEach(function(field) {
-                    var translator = outlook2google[field];
-                    if (typeof translator === 'function') {
-                        data.push(translator(record));
-                    } else {
-                        data.push(record[translator]);
-                    }
-                });
-                return data;
-            }
-        }))
-        .pipe(csv.stringify({
-            rowDelimiter: 'windows'
-        }))
-        .pipe(stream.Transform({
-            readableObjectMode: true,
-            transform: function(chunk, encoding, done) {
-                this.push(chunk.toString());
-                done();
-            }
-        }))
-        .pipe(iconv.encodeStream(encodings.google, {
-            addBOM: true
-        }))
-        .pipe(fs.createWriteStream(untildify(argv[3])));
+    // convert the CSV file
+    convert(argv[2], argv[3]);
 })();
